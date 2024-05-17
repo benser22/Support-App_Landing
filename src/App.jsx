3642d2e51e08c2fc07b20ee015a2dc3646271e24
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-05-17 12:45:35 -0300

    Feat(Loader): lazy mode incorporated to Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { Suspense, lazy } from 'react';
+import Loader from './components/Loader/Loader';
 import Navbar from './components/Navbar/Navbar';
 import Layout from './components/ui/Layout';
-import Hero from './components/Hero/Hero';
 import Supports from './components/Sections/Supports';
 import About from './components/Sections/About';
 import Features from './components/Sections/Features';
@@ -9,13 +10,18 @@ import Prices from './components/Sections/Prices';
 import Testimonials from './components/Sections/Testimonials';
 import Last from './components/Sections/Last';
 import Footer from './components/Footer/Footer';
+// import Hero from './components/Hero/Hero';
+
+const LazyHero = lazy(() => import('./components/Hero/Hero'));
 
 function App() {
   return (
     <main>
       <Navbar />
       <Layout maxWidth="max-w-[1440px]">
-        <Hero />
+        <Suspense fallback={<Loader />}>
+          <LazyHero />
+        </Suspense>{' '}
         <Supports />
         <About />
         <Features />
